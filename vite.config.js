7ffcfacd5f2364336ebe35c3ab3fb130d5a855c6
Sasan Jaghori <jaghori.sasan@gmail.com>
2022-09-10 19:31:01 +0200

    * setup project build vite config, switches back to npm

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { resolve, join } from 'path'
 
 export default defineConfig({
-  base: './',
+  root: join(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist')
+  },
   resolve: {
     alias: {
       '@styles': resolve(__dirname, './src/styles/')
